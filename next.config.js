d3179e303d563a12fa85a5fb8c267a9c6de1a1d0
Dounhuward B. Caparas <caparasdounhuward@gmail.com>
2024-07-10 21:26:30 +0800

    add yml

--- next.config.js
+++ next.config.js
@@ -2,16 +2,18 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'out',
-  }
-   
-  module.exports = nextConfig
+  // Change output directory from 'out' to 'dist'
+  distDir: 'dist',
+ 
+  // Uncomment and adjust as needed:
+  // Output format for static site export
+  // output: 'export',
+ 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+}
+ 
+module.exports = nextConfig;
